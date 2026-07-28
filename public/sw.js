7cdbe594f6ac40974ccff8ace2457c2ce287ca2c
giohannimartina-sudo <giohannimartina@gmail.com>
2026-07-28 22:00:09 +0200

    GIO_Business_Planner_PRO_Mobile_TEST_003

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,4 @@
-const CACHE='gio-business-planner-pro-v2';
-const ASSETS=['/','/planner.html','/manifest.webmanifest','/gio-logo-192.png','/gio-logo-512.png','/favicon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/planner.html'))));});
+const CACHE='gio-planner-mobile-test003';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/planner.html','/gio-mobile-test003.css?v=003','/gio-mobile-test003.js?v=003'])).catch(()=>{}))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))})
